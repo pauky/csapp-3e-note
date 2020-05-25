18350b6aa55d0275209177ec6d715c9fad7f8516
杨润炜 <runweiyang@tuputech.com>
2020-05-25 23:01:39 +0800

    feat(chapter02): add 2.94

---
+++ chapter02/2.94.c
@@ -0,0 +1,38 @@
+/*
+ * ref: https://dreamanddead.gitbooks.io/csapp-3e-solutions/chapter2/2.94.html 
+ */
+#include <stdio.h>
+#include <assert.h>
+#include "float-twice.h"
+
+float_bits float_twice(float_bits f)
+{
+    unsigned sig = f >> 31;
+    unsigned exp = f >> 23 & 0xFF;
+    unsigned frac = f & 0x7FFFFF;
+
+    int is_NAN_or_oo = (exp == 0xFF);
+    if (is_NAN_or_oo)
+    {
+        return f;
+    }
+
+    if (exp == 0)
+    {
+        /* Denormalized */
+        frac <<= 1;
+    }
+    else if (exp == 0xFF - 1)
+    {
+        /* twice to oo */
+        exp = 0xFF;
+        frac = 0;
+    }
+    else
+    {
+        /* Normalized */
+        exp += 1;
+    }
+
+    return sig << 31 | exp << 23 | frac;
+}
